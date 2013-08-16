31e2ec2b2f960d03941c29ea2e403ecfb0188136
Eden Duthie <eduthie@gmail.com>
2013-08-16 16:27:50 +1000

    completed dynamic header

---
+++ meteorhq/client/controller/header.js
@@ -0,0 +1,18 @@
+Template.header.helpers({
+	isActive : function(key) {
+		var currentPage = Template.header.getCurrentPage();
+	    if( key === currentPage ) return 'active';
+	    else return '';
+	}
+});
+
+Template.header.getCurrentPage = function() {
+	var currentUrl = document.URL;
+	if( currentUrl ) {
+		var splitUrl = currentUrl.split('/');
+		if( splitUrl && splitUrl.length >= 4 )
+		{
+			return splitUrl[3];
+		}
+	}
+}
